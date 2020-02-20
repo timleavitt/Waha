ee7cda3e111042b050fa6d36636b358c0541bb2f
trentcowden <37824478+trentcowden@users.noreply.github.com>
2020-02-20 14:10:28 -0800

    added download button and function call on press

--- components/LessonItem.js
+++ components/LessonItem.js
@@ -14,14 +14,29 @@ function LessonItem(props) {
     }
 
     return(
-        <TouchableOpacity style={styles.lessonItem} onPress={props.onLessonSelect}>
-            <View style={styles.icon}><Ionicons name={isComplete ? "ios-arrow-dropdown-circle" : "ios-arrow-dropdown"} size={30}/></View>
-            
-            <View styles={styles.titleContainer}>
-                <Text style={styles.title}>{props.title}</Text>
-                <Text style={styles.subtitle}>{props.subtitle}</Text>
+        <View style={styles.lessonItem}>
+            <TouchableOpacity style={styles.progresAndTitle} onPress={props.onLessonSelect}>
+                <View style={styles.icon}>
+                    <Ionicons 
+                        name={isComplete ? "ios-arrow-dropdown-circle" : "ios-arrow-dropdown"} 
+                        size={30}
+                    />
+                </View>
+                <View styles={styles.titleContainer}>
+                    <Text style={styles.title}>{props.title}</Text>
+                    <Text style={styles.subtitle}>{props.subtitle}</Text>
+                </View>
+            </TouchableOpacity>
+            <View style={styles.icon}>
+                <Ionicons.Button 
+                    name={"md-cloud-download"} 
+                    size={30}
+                    onPress={props.downloadLesson}
+                    backgroundColor="rgba(0,0,0,0)"
+                    color="black"
+                 />
             </View>
-        </TouchableOpacity>
+        </View>
     )
 }
 
@@ -32,7 +47,7 @@ const styles = StyleSheet.create({
         borderColor: "black",
         borderWidth: 2,
         margin: 5,
-        justifyContent: "flex-start",
+        justifyContent: "space-between",
         flexDirection: "row",
     },
     title: {
@@ -53,6 +68,13 @@ const styles = StyleSheet.create({
     icon: {
         justifyContent: "center",
         marginHorizontal: 10
+    },
+    progresAndTitle: {
+        justifyContent: "flex-start",
+        flexDirection: 'row',
+        alignContent: "center"
+    },
+    downloadButton: {
     }
 })
 
