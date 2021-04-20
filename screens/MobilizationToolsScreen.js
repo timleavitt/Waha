18b0e1491bf0413ca230ea2e2a22ef13bcb31d34
trentcowden <37824478+trentcowden@users.noreply.github.com>
2021-04-20 11:25:31 -0700

    new mob tools share system

--- screens/MobilizationToolsScreen.js
+++ screens/MobilizationToolsScreen.js
@@ -1,5 +1,14 @@
-import React, { useEffect } from 'react'
-import { Alert, Clipboard, StyleSheet, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  Clipboard,
+  Platform,
+  Share,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
+import SnackBar from 'react-native-snackbar-component'
 import { connect } from 'react-redux'
 import BackButton from '../components/standard/BackButton'
 import Blurb from '../components/standard/Blurb'
@@ -12,7 +21,7 @@ import {
   activeGroupSelector
 } from '../redux/reducers/activeGroup'
 import { colors } from '../styles/colors'
-import { getLanguageFont } from '../styles/typography'
+import { getLanguageFont, StandardTypography } from '../styles/typography'
 
 function mapStateToProps (state) {
   return {
@@ -39,6 +48,8 @@ const MobilizationToolsScreen = ({
   areMobilizationToolsUnlocked,
   groups
 }) => {
+  const [showSnackbar, setShowSnackbar] = useState(false)
+
   /** useEffect function that sets the navigation options for this screen. */
   useEffect(() => {
     setOptions({
@@ -61,40 +72,104 @@ const MobilizationToolsScreen = ({
             : translations.mobilization_tools.mobilization_tools_pre_unlock
         }
       />
-      <Separator />
-      <WahaItem
-        title={
-          areMobilizationToolsUnlocked
-            ? translations.mobilization_tools.view_code_button_label
-            : translations.mobilization_tools.unlock_mt_button_label
+      {areMobilizationToolsUnlocked ? (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 30,
+              borderRadius: 20,
+              // borderWidth: 1.5,
+              backgroundColor: colors.porcelain,
+              borderColor: colors.geyser,
+              borderBottomWidth: 4
+            }}
+            onPress={() => {
+              setShowSnackbar(true)
+              setTimeout(() => setShowSnackbar(false), 1500)
+              Clipboard.setString('281820')
+            }}
+          >
+            <Text
+              style={StandardTypography(
+                { font, isRTL },
+                'h4',
+                'Regular',
+                'center',
+                colors.shark
+              )}
+            >
+              {translations.mobilization_tools.unlock_code_label}
+            </Text>
+            <Text
+              style={StandardTypography(
+                { font, isRTL },
+                'h1',
+                'Bold',
+                'center',
+                colors.shark
+              )}
+            >
+              2 8 1 8 2 0
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'absolute',
+              right: -35 * scaleMultiplier
+            }}
+            onPress={() =>
+              Share.share({
+                message:
+                  translations.mobilization_tools
+                    .mobilization_tools_share_message
+              })
+            }
+          >
+            <Icon
+              name={Platform.OS === 'ios' ? 'share-ios' : 'share-android'}
+              color={colors.tuna}
+              size={30 * scaleMultiplier}
+            />
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={{ width: '100%' }}>
+          <Separator />
+          <WahaItem
+            title={translations.mobilization_tools.unlock_mt_button_label}
+            onPress={() => navigate('MobilizationToolsUnlock')}
+          >
+            <Icon
+              name={isRTL ? 'arrow-left' : 'arrow-right'}
+              color={colors.tuna}
+              size={50 * scaleMultiplier}
+            />
+          </WahaItem>
+          <Separator />
+        </View>
+      )}
+      <SnackBar
+        visible={showSnackbar}
+        textMessage={
+          translations.information &&
+          translations.information.copied_to_clipboard
         }
-        onPress={
-          areMobilizationToolsUnlocked
-            ? () =>
-                Alert.alert(
-                  translations.mobilization_tools.mt_code_title,
-                  '281820',
-                  [
-                    {
-                      text: translations.general.copy_to_clipboard,
-                      onPress: () => Clipboard.setString('281820')
-                    },
-                    {
-                      text: translations.general.close,
-                      onPress: () => {}
-                    }
-                  ]
-                )
-            : () => navigate('MobilizationToolsUnlock')
-        }
-      >
-        <Icon
-          name={isRTL ? 'arrow-left' : 'arrow-right'}
-          color={colors.tuna}
-          size={50 * scaleMultiplier}
-        />
-      </WahaItem>
-      <Separator />
+        messageStyle={{
+          color: colors.white,
+          fontSize: 24 * scaleMultiplier,
+          fontFamily: font + '-Black',
+          textAlign: 'center'
+        }}
+        backgroundColor={colors.apple}
+      />
     </View>
   )
 }
