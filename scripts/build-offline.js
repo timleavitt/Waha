ce9cc9c0c42a08d87e4be259d797ab2eafa61e5f
Tim Leavitt <timleavitt@users.noreply.github.com>
2021-07-25 14:31:28 -0400

    Build firebase bundle
    Note: requires service-account.json with firebase admin service account in scripts

--- scripts/build-offline.js
+++ scripts/build-offline.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 
-import db from '../firebase/db';
+import "firebase/firestore";
 import languages from '../languages';
 import { getAssetInfo } from '../assetInfo';
 import { requestPromise, spawnPromise } from './util';
-import "firebase/firestore";
 
 const fs = require('fs');
+// NOTE: must initialize this with a service account for the target Firebase environment
+const serviceAccount = require("./service-account.json");
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://" + serviceAccount.project_id + ".firebaseio.com"
+});
+
+const firestore = admin.firestore();
 
 const bundledLanguages = process.argv.slice(2);
 process.env.BUNDLED_LANGUAGES = bundledLanguages.join(',');
 const releaseChannel = "offline-"+process.env.BUNDLED_LANGUAGES.replace(',','');
 
+// Clear existing downloads folder
+const targetDirectory = './assets/downloaded';
+if (fs.existsSync(targetDirectory)) {
+    fs.rmdirSync(targetDirectory,{
+        recursive: true,
+        force: true
+    });
+}
+fs.mkdirSync(targetDirectory);
+
+const bundle = firestore.bundle(releaseChannel);
 Promise.all(bundledLanguages.map((value, _, array) => {
     // Determine full list of assets to download
     const lang = languages.find(lang => lang.languageCode == value);
@@ -19,18 +39,16 @@ Promise.all(bundledLanguages.map((value, _, array) => {
         throw new Error("Invalid language: " + value);
     }
     const promises = [];
-    promises.push(db.collection('languages').doc(value).get().then(doc => {
+    promises.push(firestore.collection('languages').doc(value).get().then(doc => {
         if (doc.exists && doc.data()) {
-            // TODO: Queue download of firebase bundle for language
-            // console.log(doc.data());
+            bundle.add(doc);
         }
     }));
-    promises.push(db.collection('sets').where('languageID','==',value).get().then(result => {
+    promises.push(firestore.collection('sets').where('languageID','==',value).get().then(result => {
         const assets = [];
+        bundle.add(`language-${value}`,result);
         result.docs.map(set => {
             if (set.exists && set.data()) {
-                // TODO: Queue download of firebase bundle for set
-                // console.log(set.data());
 
                 // Queue download of video/audio for lesson if available
                 set.data().lessons.forEach(lesson => {
@@ -61,24 +79,25 @@ Promise.all(bundledLanguages.map((value, _, array) => {
     coalesce(results);
     return files;
 }).then(files => {
-    // Clear existing downloads folder
-    const targetDirectory = './assets/downloaded';
-    if (fs.existsSync(targetDirectory)) {
-        fs.rmdirSync(targetDirectory,{
-            recursive: true,
-            force: true
-        });
-    }
-    fs.mkdirSync(targetDirectory);
+    // Write bundle to file
+    const file = `./assets/downloaded/all.firebase-bundle`;
+    fs.writeFile(file, bundle.build(),  "binary", function(err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(`Built Firebase bundle: ${file}`);
+        }
+    });
 
-    // Download the files
+    // Download audio/video files
     const fileDownloads = [];
     console.log('Downloading audio/video files...');
     files.forEach(remoteFileName => {
         // NOTE: Path is relative to project root, not scripts folder
-        const localFileName = targetDirectory + "/" + remoteFileName.split('/').pop().split('?')[0];
+        const localFileName = targetDirectory + "/" + decodeURIComponent(remoteFileName.split('/').pop().split('?')[0]);
         fileDownloads.push(requestPromise(remoteFileName).then(response => {
             const buffer = Buffer.from(response.body);
+            fs.mkdirSync(localFileName.split('/').slice(0,-1).join('/'), {recursive: true});
             const file = fs.createWriteStream(localFileName);
             file.write(buffer);
             file.close();
